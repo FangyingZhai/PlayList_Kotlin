2cdcd8a9ef431d1a67acd73675a93f27e094f0f2
Fangying <zhaifangying@wustl.edu>
2019-03-20 01:35:54 -0500

    delete

--- Playlist/app/src/main/java/com/zhai/fangying/playlist/Callback.kt
+++
@@ -1,6 +0,0 @@
-package com.zhai.fangying.playlist
-
-public interface Callback<T> {
-    fun onPostResult(data: T)
-    fun onError()
-}
--- Playlist/app/src/main/java/com/zhai/fangying/playlist/model/Artist.kt
+++
@@ -1,6 +0,0 @@
-package com.zhai.fangying.playlist.model
-
-data class Artist (
-    val name : String,
-    val imgUrl : String
-)
